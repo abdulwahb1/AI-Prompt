e8c882b611fd910137d1913ebd177fd4d499f482
Abdul Wahab <abdulwabby@gmail.com>
2024-01-11 02:19:25 +0500

    user-create fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-import Users from '@models/user';
+import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
@@ -14,7 +14,7 @@ const handler = NextAuth({
   callbacks: {
     async session({ session }) {
       // store the user id from MongoDB to session
-      const sessionUser = await Users.findOne({ email: session.user.email });
+      const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
 
       return session;
@@ -24,7 +24,7 @@ const handler = NextAuth({
         await connectToDB();
 
         // check if user already exists
-        const userExists = await Users.findOne({ email: profile.email });
+        const userExists = await User.findOne({ email: profile.email });
 
         // if not, create a new document and save user in MongoDB
         if (!userExists) {
