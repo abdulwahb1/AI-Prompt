495086ff252cc90620d2603a2fee3071747ab658
Abdul Wahab <abdulwabby@gmail.com>
2024-01-11 00:23:47 +0500

    First commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'lh3.googleusercontent.com',
+              // Optionally, you can specify a port and pathname
+          },
+      ],
+  },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+  module.exports = nextConfig
