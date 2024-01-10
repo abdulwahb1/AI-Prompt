2d0be8027b9f3e5189fbab0f1b3fa7031e2f619b
Abdul Wahab <abdulwabby@gmail.com>
2024-01-11 03:11:49 +0500

    added-loading

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,26 +4,25 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts",
-  };
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-  const RootLayout = ({ children }) => (
-    <html lang='en'>
-      <body>
-        <Provider>
-          <div className='main'>
-            <div className='gradient' />
-          </div>
-  
-          <main className='app'>
-            <Nav />
-            {children}
-          </main>
-          </Provider>
-      </body>
-    </html>
-  );
-  
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-export default RootLayout
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
